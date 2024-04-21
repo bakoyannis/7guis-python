ae22e122f3ae139b5df33ec9e321ca7869269266
Thomas Bakoyannis <83982517+bakoyannis@users.noreply.github.com>
2024-04-21 16:03:36 +0300

    Update timer.py

--- timer.py
+++ timer.py
@@ -27,41 +27,41 @@ class Application(tk.Tk):
 
         self.d_time = 10
         self.elapsed_ms = 0
-        self.timer_id = None
-        self.d_slider = ttk.Scale(self, from_=0, to=100, orient="horizontal", command=self.slider_update)
+        self.timer_id = None # Initialize variable for the id of the timer
+        self.d_slider = ttk.Scale(self, from_=0, to=100, orient="horizontal", command=self.update_duration)
         self.d_slider.set(10)
         self.d_slider.grid(row=2, column=1, sticky="nswe", padx=10)
 
         self.reset_button = ttk.Button(self, text="Reset", command=self.reset)
         self.reset_button.grid(row=3, column=0, columnspan=2, sticky="nswe", padx=10, pady=10)
 
-        self.start_timer()
+        # Start the timer
+        self.timer()
 
-    def start_timer(self):
-        self.update_timer()
-
-    def update_timer(self): 
+    # Main timer function
+    def timer(self): 
         self.et_time['text'] = "{:.1f}".format(self.elapsed_ms/1000) + "s"  # Display elapsed time with one decimal place
-
         diff =  self.d_time - (self.elapsed_ms/1000)
 
         if not (self.elapsed_ms/1000) >= self.d_time:
             self.progress_bar["value"] = ((self.d_time - diff) / self.d_time) * 100 if self.d_time else 0
-            # Update the timer every 1ms
             self.elapsed_ms += 1
-            self.timer_id = self.after(1, self.update_timer)
+            # Run every 1 ms
+            self.timer_id = self.after(1, self.timer)
         else:
             self.progress_bar["value"] = 100
 
+    # Reset the timer
     def reset(self):
         self.elapsed_ms = 0
-        self.after_cancel(self.timer_id)
-        self.start_timer()
+        self.after_cancel(self.timer_id) # Stop all instances of timer
+        self.timer()
 
-    def slider_update(self, value):
+    # Update the duration based on the slider
+    def update_duration(self, value):
         self.d_time = float(value)
         if self.d_time > self.elapsed_ms/1000 and self.progress_bar["value"] == 100:
-            self.start_timer()
+            self.timer()
 
 if __name__ == "__main__":
     app = Application()
